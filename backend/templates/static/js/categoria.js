0d12d928c60f02948aed790e2a8536e7e7085457
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2024-06-28 22:27:18 +0000

    modificando a forma do envio do fomulário da filtragem pro subcategoria, marca e ingredientes

--- backend/templates/static/js/categoria.js
+++ backend/templates/static/js/categoria.js
@@ -3,17 +3,23 @@ const menu = document.querySelector('#menu_ul1');
 const toggleMenuButtonct = document.querySelector('.button2');
 const menu2 = document.querySelector('#menu_ul2');
 const toggleMenuButtonmc = document.querySelector('.button3');
-const menu3 = document.querySelector('#menu_ul3'); 
+const menu3 = document.querySelector('#menu_ul3');
 
 
-toggleMenuButton.addEventListener('click', () =>{
+toggleMenuButton.addEventListener('click', () => {
    menu.style.display = menu.style.display === 'none' ? 'block' : 'none'
 });
 
-toggleMenuButtonct.addEventListener('click', () =>{
+toggleMenuButtonct.addEventListener('click', () => {
    menu2.style.display = menu2.style.display === 'none' ? 'block' : 'none'
 });
 
-toggleMenuButtonmc.addEventListener('click', () =>{
+toggleMenuButtonmc.addEventListener('click', () => {
    menu3.style.display = menu3.style.display === 'none' ? 'block' : 'none'
 });
+
+
+function enviar_formulario() {
+   const FORMULARIO = document.querySelector("#filtrar_produtos");
+   FORMULARIO.submit()
+};
